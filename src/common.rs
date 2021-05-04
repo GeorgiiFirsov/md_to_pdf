140a9a502d4fdeaf70551133e591207f84f675da
GeorgyFirsov <gfirsov007@gmail.com>
2021-05-04 13:35:58 +0300

    Command line parsing

---
+++ src/common.rs
@@ -0,0 +1,23 @@
+// This file contains some constants and other commonly used in the application stuff
+//
+
+// Error message displayed when input file name does not have *.md extension
+pub(crate) const INCORRECT_INPUT_FILE: &str = "input file must have \"md\" extension";
+
+// Error message displayed when output file name does not have *.pdf extension
+pub(crate) const INCORRECT_OUTPUT_FILE: &str = "output file must have \"pdf\" extension";
+
+// Error message displayed when trace subscriber cannot be set
+pub(crate) const CANNOT_SET_TRACE_SUBSCRIBER: &str = "cannot set a subscriber for tracing";
+
+// Markdown file extension
+pub(crate) const MD_EXTENSION: &str = ".md";
+
+// PDF file extension
+pub(crate) const PDF_EXTENSION: &str = ".pdf";
+
+// Default markdown file name
+pub(crate) const DEFAULT_MD_NAME: &str = "input.md";
+
+// Default PDF file name
+pub(crate) const DEFAULT_PDF_NAME: &str = "output.pdf";
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+mod process_markdown;
+mod compose_pdf;
+mod common;
+
+extern crate clap;
+extern crate tracing;
+
+use clap::{App, load_yaml};
+use tracing::{error, debug, info, warn, Level};
+use common::{INCORRECT_INPUT_FILE, INCORRECT_OUTPUT_FILE,
+             DEFAULT_MD_NAME, DEFAULT_PDF_NAME,
+             MD_EXTENSION, PDF_EXTENSION, CANNOT_SET_TRACE_SUBSCRIBER};
+
+
+fn main() {
+    //
+    // Setup trace service
+    //
+
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG).finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect(CANNOT_SET_TRACE_SUBSCRIBER);
+
+    //
+    // Read and parse command line arguments
+    //
+
+    let yaml_cli_config = load_yaml!("../config/cli.yaml");
+    let matches = App::from(yaml_cli_config).get_matches();
+
+    let input_file = matches.value_of("input").unwrap_or(DEFAULT_MD_NAME);
+    let output_file = matches.value_of("output").unwrap_or(DEFAULT_PDF_NAME);
+
+    //
+    // Verify their correctness
+    //
+
+    if !input_file.ends_with(MD_EXTENSION) {
+        std::panic::panic_any(INCORRECT_INPUT_FILE);
+    }
+
+    if !output_file.ends_with(PDF_EXTENSION) {
+        std::panic::panic_any(INCORRECT_OUTPUT_FILE);
+    }
+
+    debug!("input_file == {}", input_file);
+    debug!("output_file == {}", output_file);
+
+}
