a7d22114298b43391d8b3e48924f41dafb927b9b
GeorgyFirsov <gfirsov007@gmail.com>
2021-05-06 21:19:07 +0300

    Styles added for headers, quotes, lists, underlined and crossed-out texts

--- src/compose_pdf.rs
+++ src/compose_pdf.rs
@@ -2,9 +2,11 @@
 //
 
 extern crate wkhtmltopdf;
+extern crate tracing;
 
 
 use wkhtmltopdf::pdf;
+use tracing::{debug, info};
 
 use crate::common::{CANNOT_INIT_PDF_APP, CANNOT_RENDER_PDF,
                     CANNOT_SAVE_PDF};
@@ -15,12 +17,18 @@ fn convert_html_to_pdf(filename: &str, html_content: &str) {
     let pdf_app = pdf::PdfApplication::new()
         .expect(CANNOT_INIT_PDF_APP);
 
+    debug!("PDF application is initialized");
+
     // mut is necessary for method 'save'
     let mut pdf_output = pdf_app.builder()
         .orientation(pdf::Orientation::Portrait)
         .build_from_html(html_content)
         .expect(CANNOT_RENDER_PDF);
 
+    info!("PDF file rendered successfully");
+
     pdf_output.save(filename)
         .expect(CANNOT_SAVE_PDF);
+
+    info!("PDF file successfully saved to {}", filename);
 }
--- src/process_markdown.rs
+++ src/process_markdown.rs
@@ -1,10 +1,14 @@
-// This file contains Markdown parser implementation
+// This file contains Markdown parser and converter implementation
 //
 
 extern crate comrak;
+extern crate tracing;
+extern crate phf;
 
 
 use std::fs;
+use phf::phf_map;
+use tracing::{debug, info};
 use comrak::{markdown_to_html, ComrakOptions};
 
 use crate::common::ERROR_READING_MD_CONTENT;
@@ -12,6 +16,23 @@ use crate::common::ERROR_READING_MD_CONTENT;
 
 // CSS style used to format intermediate HTML representation
 const CSS_STYLE: &'static str = include_str!("../styles/pretty_pdf.css");
+
+// HTML high-level tags
+const HTML_HEAD_STYLE_BEGIN: &'static str = "<head><style>";
+const HTML_HEAD_STYLE_END: &'static str = "</style></head>";
+const HTML_BODY_BEGIN: &'static str = "<body>";
+const HTML_BODY_END: &'static str = "</body>";
+
+// Mapping between initial tokens in HTML and their replacements
+const TOKEN_MAPPING: phf::Map<&'static str, &'static str> = phf_map!(
+    "blockquote"    => "blockquote class=\"quote\"",
+    "<p>~~"         => "<p class=\"crossed_out_text\">",
+    "~~\n~~"        => "<br />\n",
+    "~~"            => "",
+    "<p>~"          => "<p class=\"underlined_text\">",
+    "~\n~"          => "<br />\n",
+    "~"             => ""
+);
 
 
 pub(crate)
@@ -23,14 +44,26 @@ fn convert_markdown_to_pretty_html(filename: &str) -> String {
     let md_content = fs::read_to_string(filename)
         .expect(ERROR_READING_MD_CONTENT);
 
-    let html = markdown_to_html(
+    let mut raw_html = markdown_to_html(
         &md_content, &ComrakOptions::default());
 
+    info!("File {} converted in HTML successfully", filename);
+
     //
-    // Embed a style into HTML and return pretty HTML text
+    // Embed a styles into HTML
     //
 
-    // TODO: embed CSS styles
+    for (&token, &replacement) in TOKEN_MAPPING.entries() {
+        raw_html = raw_html.replace(token, replacement);
+    }
 
-    html
+    info!("HTML tokens replaced with their styled analogues");
+    debug!("Style: {}", CSS_STYLE);
+
+    //
+    // Compose everything together and return
+    //
+
+    [HTML_HEAD_STYLE_BEGIN, CSS_STYLE, HTML_HEAD_STYLE_END,
+        HTML_BODY_BEGIN, &raw_html, HTML_BODY_END].join("")
 }
